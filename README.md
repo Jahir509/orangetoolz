86a0ff643382eb9bf1d4ef1f06e065c396fc540f
jahir509 <me.jahirahmed@gmail.com>
2023-05-05 12:13:22 +0600

    Update: README.md

--- README.md
+++ README.md
@@ -1,6 +1,11 @@
 # orangetoolz
 orange-toolz test for node js engineer
+
 #Project Documentation
+
+technology: 
+node - 16.13.0,
+mongodb - 4.4.6
 
 for importing data: run `npm run import <csv file name>` | `node import.js <csv filename>`
 
@@ -8,3 +13,5 @@ for exporting data: run `npm run export` | `node export.js`
 
 -- this command will generate valid and invalid customer in separate csv files
 
+-- for valid customer filename: valid-customer-batch-(number).csv
+-- for invalid customer filename: invalid-customer-batch-(number).csv
--- exportCsv.js
+++ exportCsv.js
@@ -1,10 +1,13 @@
 
 const fs = require('fs');
 
+/**
+ * Function to process CSV file
+ * **/
 async function exportToCSV(data, filename) {
     return new Promise((resolve, reject) => {
         const stream = fs.createWriteStream(filename);
-
+        // writing data into csv file
         stream.write('firstname,lastname,city,state,zipcode,phone,email,ip\n');
         data.forEach((customer)=> {
             stream.write(`${customer.firstname},${customer.lastname},${customer.city},${customer.state},${customer.zipcode},${customer.phone},${customer.email},${customer.ip}\n`)
